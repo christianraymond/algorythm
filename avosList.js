c1cdf785abac1d1ff73906685325ec9929b61b1d
christianraymond <ombenisn.raymond@gmail.com>
2017-05-10 15:09:22 +0200

    mocha not setted proparly

---
+++ avosList.js
@@ -0,0 +1,3 @@
+var deal = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+var Alldeals = deal.split(",");
+console.log(Alldeals);
---
+++ avosTest/avosListTest.js
@@ -0,0 +1,5 @@
+describe('The avosDeals function', function(){
+  it('It should return the length of my list', function(){
+    assert.equal(avosDeals, 4);
+  });
+});
